3d846281e7f45b054238991c96fefee303663ce7
lalitbansal40 <165453425+lalitbansal40@users.noreply.github.com>
2024-06-18 22:14:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
+const Shopify = require("shopify-api-node");
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = 5000;
-
-const Shopify = require("shopify-api-node");
+app.use(cors({ origin: "*" }));
 let shop = "messold101.myshopify.com";
 let token = "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";
 let key = "ce5ef30c1e4768394af59456800262b3";
@@ -13,8 +13,8 @@ const shopify = new Shopify({
   password: token,
 });
 
-
 app.get("/api/products", async (req, res) => {
+  console.log("hello");
   const products = await shopify.product.list();
   res.send(products);
 });
